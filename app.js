799d083bf1613285ffb290ed85ac3c79063662be
RyanAshB <ryanash.b@gmail.com>
2023-11-12 23:30:54 -0500

    Exercise 3 Complete

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+ 
     document.getElementById('searchButton').addEventListener('click', function () {
+        const userInput = document.getElementById('searchInput').value.trim();
+        const sanitizedInput = sanitizeInput(userInput);
 
-        fetch('superheroes.php')
+
+        fetch(`superheroes.php?query=${sanitizedInput}`)
             .then(response => response.json())
             .then(data => {
 
-                let alertMessage = '';
-                data.forEach(superhero => {
-                    alertMessage += `${superhero.alias}\n`;
-                });
-                alert(alertMessage);
+                displayResult(data);
+
             })
-            .catch(error => console.error('Error in search', error));
+            .catch(error => console.error('Error:', error));
     });
 });
+
+function sanitizeInput(input) {
+    return encodeURIComponent(input);
+}
+
+function displayResult(data) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    const userInput = document.getElementById('searchInput').value.trim();
+
+
+    if (userInput === '') {
+        resultDiv.innerHTML = '<ul>' +
+            data.map(superhero => `<li>${superhero.alias}</li>`).join('') +
+            '</ul>';
+    } else {
+        if (data.length > 0) {
+            let matchFound = false;
+
+         
+            for (const superhero of data) {
+                if (superhero.alias.toLowerCase() === userInput.toLowerCase() || superhero.name.toLowerCase() === userInput.toLowerCase()) {
+                    resultDiv.innerHTML = `
+                        <h1 class="resultHeading">Result</h1>
+                        <h3>${superhero.alias}</h3>
+                        <h4>${superhero.name}</h4>
+                        <p>${superhero.biography}</p>
+                    `;
+
+                    matchFound = true;
+                    break;  
+
+                }
+            }
+
+            if (!matchFound) {
+                resultDiv.innerHTML = `<h2 class="noSups">Superhero not found</h2>`;
+            }
+        } else {
+
+            resultDiv.innerHTML = `<h2 class="noSups">Superhero not found</h2>`;
+        }
+    }
+}
 
