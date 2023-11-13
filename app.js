0b232a5ce4426bfaaa7874995519442da709952c
RyanAshB <ryanash.b@gmail.com>
2023-11-12 22:15:34 -0500

    Excercise 2 Complete

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ document.getElementById("searchButton").addEventListener("click", function() {
     xhr.open("GET", "superheroes.php", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            // Parse the JSON response
             var superheroes = JSON.parse(xhr.responseText);
 
-            // Create a string with superhero aliases
             var aliases = superheroes.map(function(superhero) {
                 return superhero.alias;
+                
             }).join("\n");
 
-            // Display the superhero aliases in an alert
             alert(aliases);
         }
     };
